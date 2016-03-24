8591b356c0db596f2c81f042d390e2e6c19f227d
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-03-24 15:17:24 +0300

    Add project for .net 4.0

---
+++ dotNet40ClassLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace dotNet40ClassLibrary
+{
+    public class Class1
+    {
+	    public void Do()
+	    {		    
+	    }
+    }
+}
---
+++ dotNet40NUnitTests/Class1Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace dotNet40NUnitTests
+{
+	using dotNet40ClassLibrary;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class Class1Tests
+	{
+		[Test]
+		public void TestDo()
+		{
+			new Class1().Do();
+		}
+	}
+}

68df58d8ba4b122d74371f06cb9b2a4ac731a3e3
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-04-04 12:44:03 +0300

    Add test cases

---
+++ dotNet20NUnitTests/PathProviderTests.cs
@@ -0,0 +1,33 @@
+﻿namespace dotNet20NUnitTests
+{
+	using System.Collections.Generic;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class PathProviderTests
+	{
+		public static IEnumerable<TestCaseData> TestDataList
+		{
+			get
+			{
+				var names = new[] {
+					"simple-types.j.xml",
+					"simple-types.k.xml",
+					"simple-types.l.xml",
+					"simple-types.m.xml",
+				};
+
+				foreach (var name in names)
+				{
+					yield return new TestCaseData(name).SetName(name);
+				}				
+			}
+		}
+
+		[Test, TestCaseSource("TestDataList")]
+		public void ReproduceTeamCityReportingBug(string data)
+		{
+		}
+	}
+}

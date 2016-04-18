8f124ea97cf5e17c370f198e0986db76ee0cf071
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-04-18 12:27:25 +0300

    Notify about CurrentDirectory and bitness

--- dotNet20ClassLibrary/Class1.cs
+++ dotNet20ClassLibrary/Class1.cs
@@ -1,9 +1,13 @@
 ﻿namespace dotNet20ClassLibrary
 {
+	using System;
+
     public class Class1
     {
 	    public void Do()
 	    {		    
+			Console.WriteLine($"CurrentDirectory={Environment.CurrentDirectory}");
+			Console.WriteLine($"Bitness={IntPtr.Size * 8}");
 	    }
     }
 }

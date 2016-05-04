bb0f6598183cc1728a82c9dc7d9ca8dc48556486
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-05-04 13:00:31 +0300

    Add dotcover tests

---
+++ dotNet40NUnitTests/DotCoverTests.cs
@@ -0,0 +1,38 @@
+﻿namespace dotNet40NUnitTests
+{
+	using System;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class DotCoverTests
+	{
+		[Test]
+		public void TestToInt()
+		{
+			Assert.AreEqual(0, ToInt(double.NaN), "NaN string");
+		}
+
+		public static int ToInt(object userInput, int defaultValue = 0)
+		{
+			var result = defaultValue;
+			if (userInput == null)
+			{
+				return result;
+			}
+
+			double value;
+			if (!double.TryParse(userInput.ToString().Trim(), out value))
+			{
+				return result;
+			}
+
+			if (value <= int.MaxValue && value >= int.MinValue)
+			{
+				result = Convert.ToInt32(Math.Truncate(value));
+			}
+
+			return result;
+		}
+	}
+}

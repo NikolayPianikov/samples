d9646bc71640a94e920afffb6bb32811bb8ae6b2
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-05-06 17:22:38 +0300

    Move some files

---
+++ dotNet20ClassLibraryAndVSTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace dotNet20ClassLibraryAndVSTests
+{
+    using dotNet20ClassLibrary;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new Class1().Do();
+        }
+    }
+}

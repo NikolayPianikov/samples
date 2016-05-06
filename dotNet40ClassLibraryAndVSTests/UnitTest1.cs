7c998bfd99b04a61f9e98ecb2253699d3955a2b5
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-05-06 15:42:41 +0300

    Add vs tests

---
+++ dotNet40ClassLibraryAndVSTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace dotNet40ClassLibraryAndVSTests
+{
+	using dotNet40ClassLibrary;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			new Class1().Do();
+		}
+	}
+}

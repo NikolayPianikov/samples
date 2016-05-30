af5377184c55f7317fce25369965404a95b6ab10
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-05-30 14:27:39 +0300

    Add ru tests

---
+++ dotNet20NUnitTests/RuTests.cs
@@ -0,0 +1,20 @@
+﻿namespace dotNet20NUnitTests
+{
+	using System;
+
+	using dotNet20ClassLibrary;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class RuTests
+	{
+		[Test, Category("Sucess")]
+		public void TestDo()
+		{
+			Console.WriteLine("Привет !!!");
+			new Class1().Do();
+		}
+	}
+
+}
